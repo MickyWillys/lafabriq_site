af6fc4ce32fc3630c46df4a9acf31f7407919ef7
MickyWillys <fozeum@yahoo.fr>
2013-12-28 00:13:42 +0100

    finish user signup and configuration to french

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,11 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
+
 module LafabriqSite
   class Application < Rails::Application
+    config.i18n.default_locale = :fr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
